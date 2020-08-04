f6a32b38b6628503422d7bcfb21f7bbbf96d159d
nilesh <nilesh.katre@sterlite.com>
2020-08-04 14:26:37 +0530

    space remove

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,5 +25,4 @@ use App\Http\Controllers\API\MediaPostController;
     Route::middleware('auth:api')->post('/mediapost/search', 'API\MediaPostController@search');
     Route::middleware('auth:api')->post('/mediapost', 'API\MediaPostController@storeMediaPost');
     Route::middleware('auth:api')->get('/mediapost', 'API\MediaPostController@mediaAll');
-    Route::middleware('auth:api')->get('/mediapost/user/{uuid}', 'API\MediaPostController@showMediaPost');   
-    
+    Route::middleware('auth:api')->get('/mediapost/user/{uuid}', 'API\MediaPostController@showMediaPost');
